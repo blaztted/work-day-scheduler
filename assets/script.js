c34d2701a53401284bf9173fd9266c87a6d21699
blaztted <bbc.silva@gmail.com>
2024-01-05 18:55:57 +0000

    added bootstrap card to header and changed google font family

---
+++ assets/script.js
@@ -0,0 +1,25 @@
+<style>
+  @import
+  url('https://fonts.googleapis.com/css2?family=Quicksand&display=swap');
+</style>;
+
+// The app should:
+console.log("ding");
+
+// Display the current day at the top of the calender when a user opens the planner.
+// create dayjs property to show the date at the top
+// center the h1 and p
+
+// Present time blocks for standard business hours when the user scrolls down.
+//new div with 9-5 individual blocks
+
+// Color-code each time block based on past, present, and future when the time block is viewed.
+//if the block is in the past, the background is grey, if now red, otherwise green
+
+// Allow a user to enter an event when they click a time block
+//at the end of each block, you can click an icon to save what was written on the block
+
+//use json.stringify to store and json.parse to retreieve from local storage after page refresh
+// Save the event in local storage when the save button is clicked in that time block.
+
+// Persist events between refreshes of a page
