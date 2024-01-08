f63bc42c2739165f9a6e8916bff3af6780f994e5
blaztted <bbc.silva@gmail.com>
2024-01-08 16:55:50 +0000

    wrote displayCalendar function

--- assets/script.js
+++ assets/script.js
@@ -3,16 +3,64 @@
 // dayjs.extend(advancedFormat);
 // dayjs().format("Q Do k kk X x");
 
+//! Display the current day in the element with the id 'currentDay'
+// !create dayjs property to show the date at the top
+// !center the h1 and p
 var currentDate = dayjs().format("dddd, MMMM D, YYYY");
-// Display the current day in the element with the id 'currentDay'
+
+//store calendar events
+let calEvents = {};
+
+//
+let hourDisplay = dayjs();
+
+//Display current day
 $("#currentDay").text(currentDate);
 console.log(currentDate);
 
-// Add the rest of your code below this point
-// ...
+function displayCalendar(today, calEvents) {
+  let hourRow = dayjs(today).hour(9);
+  const cal = $("div.container");
+  cal.empty();
 
-// create dayjs property to show the date at the top
-// center the h1 and p
+  for (let i = 0; i < 10; i++) {
+    const row = $("<div>").addClass("row"); // create row for each hour
+
+    let color = "";
+    if (today.isAfter(hourRow, "hour")) {
+      color = "past";
+    } else if (today.isBefore(hourRow, "hour")) {
+      color = "future";
+    } else {
+      color = "present";
+    }
+
+    cal.append(row);
+
+    row.append($("<div").addClass("col-2 hour").text(hourRow.format("h A")));
+
+    //event text
+
+    let blockEvent = hourRow.format("hA");
+    row.append(
+      $("<textarea>").addClass(`col-8 ${color}`).text(calEvents[blockEvent])
+    );
+
+    //save btn
+
+    row.append(
+      $("<button>")
+        .addClass("col-2 saveBtn")
+        .html("<i class='far fa-save'></i>")
+        .attr("aria-label", "Save")
+        .attr("id", hourRow.format("hA"))
+    );
+
+    hourRow.add(1, "hour");
+
+    hourDisplay = dayjs();
+  }
+}
 
 // Present time blocks for standard business hours when the user scrolls down.
 //new div with 9-5 individual blocks
@@ -27,3 +75,12 @@ console.log(currentDate);
 // Save the event in local storage when the save button is clicked in that time block.
 
 // Persist events between refreshes of a page
+
+// function to save to local storage when button is clicked
+// get the values
+// store them
+// function to update classes hours change
+// get current time
+// comptare against those in your time blocks
+// update the classes
+// remember to also get values from local storage and set them on page load
